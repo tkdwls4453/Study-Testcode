621545b484a424645495a7ca0b6ae17054c89da3
Yoon321 <tkdwls734@gmail.com>
2024-02-16 23:34:47 +0900

    api 테스트를 하기 위해 http 파일 생성

--- src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
@@ -2,9 +2,11 @@ package sample.cafekiosk.spring.api.controller.order;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
 import sample.cafekiosk.spring.api.service.order.OrderService;
+import sample.cafekiosk.spring.api.service.response.OrderResponse;
 
 import java.time.LocalDateTime;
 
@@ -15,7 +17,7 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/api/v1/orders/new")
-    public void createOrder(OrderCreateRequest request) {
-        orderService.createOrder(request, LocalDateTime.now());
+    public OrderResponse createOrder(@RequestBody OrderCreateRequest request) {
+        return orderService.createOrder(request, LocalDateTime.now());
     }
 }
