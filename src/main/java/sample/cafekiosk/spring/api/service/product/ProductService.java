0c618c1621b9afd23c2c7abaee1c614a420b1734
Yoon321 <tkdwls734@gmail.com>
2024-02-19 15:21:39 +0900

    상품을 생성하는 기능 완성 및 테스트

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -2,6 +2,8 @@ package sample.cafekiosk.spring.api.service.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
@@ -10,11 +12,45 @@ import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
+import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
+
+/**
+ * readOnly = true : 읽기 전용
+ * CRUD 에서 CUD 동작 X / only Read
+ * JPA : CUD 스냅샷 저장, 변경감지 X (성능 향상)
+ *
+ * CQRS - Command / Read
+ */
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
+
+    @Transactional
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        String nextProductNumber = createNextProductNumber();
+
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.of(savedProduct);
+    }
+
+    private String createNextProductNumber() {
+        String lastProductNumber = productRepository.findLastProduct();
+
+        if (lastProductNumber == null) {
+            return "001";
+        }
+
+        int lastProductNumberInt = Integer.parseInt(lastProductNumber);
+        int nextProductNumberInt = lastProductNumberInt + 1;
+
+        return String.format("%03d", nextProductNumberInt);
+    }
 
     public List<ProductResponse> getSellingProducts(){
         List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
